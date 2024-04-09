7a324a9fe0590c23dea0c0ea79faf2109abbadb8
Wonwoo <seo2wo@naver.com>
2024-04-09 17:31:39 +0900

    시설현황 페이지 사이드 메뉴 추가

---
+++ js/menu/menu_sidebar.js
@@ -0,0 +1,109 @@
+// menu_sidebar.js
+
+document.querySelector('.menu1-1').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-1').classList.add('side-show');
+    document.querySelector('.menu1-1-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-1').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-1').classList.remove('side-show');
+    document.querySelector('.menu1-1-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-2').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-2').classList.add('side-show');
+    document.querySelector('.menu1-2-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-2').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-2').classList.remove('side-show');
+    document.querySelector('.menu1-2-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-3').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-3').classList.add('side-show');
+    document.querySelector('.menu1-3-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-3').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-3').classList.remove('side-show');
+    document.querySelector('.menu1-3-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-4').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-4').classList.add('side-show');
+    document.querySelector('.menu1-4-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-4').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-4').classList.remove('side-show');
+    document.querySelector('.menu1-4-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-5').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-5').classList.add('side-show');
+    document.querySelector('.menu1-5-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-5').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-5').classList.remove('side-show');
+    document.querySelector('.menu1-5-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-6').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-6').classList.add('side-show');
+    document.querySelector('.menu1-6-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-6').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-6').classList.remove('side-show');
+    document.querySelector('.menu1-6-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-7').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-7').classList.add('side-show');
+    document.querySelector('.menu1-7-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-7').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-7').classList.remove('side-show');
+    document.querySelector('.menu1-7-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-8').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-8').classList.add('side-show');
+    document.querySelector('.menu1-8-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-8').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-8').classList.remove('side-show');
+    document.querySelector('.menu1-8-img').classList.remove('img-show');
+}
+);
+
+document.querySelector('.menu1-9').addEventListener('mouseover', () => {
+    document.querySelector('.menu1-9').classList.add('side-show');
+    document.querySelector('.menu1-9-img').classList.add('img-show');
+}
+);
+
+document.querySelector('.menu1-9').addEventListener('mouseout', () => {
+    document.querySelector('.menu1-9').classList.remove('side-show');
+    document.querySelector('.menu1-9-img').classList.remove('img-show');
+}
+);
